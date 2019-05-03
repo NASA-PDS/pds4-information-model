1b8ec56c61ea85d99f93d69b055ab7fde909dd89
Steve Hughes <John.S.Hughes@jpl.nasa.gov>
2019-05-03 15:08:55 -0700

    190403_Bugfix_Cart_Geom_Inheritance_Problem_2 (#57)
    Dump LDDTool Version.

--- model-dmdocument/src/main/java/gov/nasa/pds/model/plugin/DMDocument.java
+++ model-dmdocument/src/main/java/gov/nasa/pds/model/plugin/DMDocument.java
@@ -104,7 +104,7 @@ public class DMDocument extends Object {
 	// x.x.x.x - 1.0 - 1.n - Build nm - first version of product will always be 1.0
 	//									Modification history will continue with 1.n
 	                         
-	static String LDDToolVersionId  = "0.2.2.4";
+	static String LDDToolVersionId  = "0.2.2.5";
 	static String classVersionIdDefault = "1.0.0.0";
 //	static String LDDToolGeometry = "Geometry";
 	static boolean PDS4MergeFlag  = false;
