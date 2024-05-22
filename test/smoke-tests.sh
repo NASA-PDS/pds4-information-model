c906374ff9f7c1be786c8b7fa82352266d01f4a2
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2024-05-22 09:54:12 -0700

    Update smoke tests to include additonal file count check

--- test/smoke-tests.sh
+++ test/smoke-tests.sh
@@ -27,6 +27,7 @@ if [ $? -ne 0 ]; then
     echo "Test 1: FAILED"
     exit $?
 fi
+echo "Test 1: SUCCESS"
 
 echo "Test 2: ldd-cart"
 rm -fr ldd-repo
@@ -35,6 +36,21 @@ git clone https://github.com/pds-data-dictionaries/ldd-cart.git ldd-repo/
 cd ldd-repo/; git submodule update --init --force --remote; cd ..
 
 # Run lddtool
-lddtool-pkg/lddtool*/bin/lddtool -lpJ -V 1L00 ldd-repo/src/*IngestLDD.xml ldd-repo/src/dependencies/ldd-*/src/*IngestLDD.xml
+lddtool-pkg/lddtool*/bin/lddtool -lpJ -V 1L00 ldd-repo/src/dependencies/ldd-*/src/*IngestLDD.xml ldd-repo/src/*IngestLDD.xml
+
+if [ $? -ne 0 ]; then
+    echo "Test 2: FAILED"
+    exit $?
+fi
+echo "Test 2: SUCCESS"
+
+# File count check
+echo "Test 3: Check file counts from files generated"
+count=$(ls -al PDS* | wc -l)
+if [ $count -ne 12 ]; then
+    echo "Test 3: FAILED"
+    exit 1
+fi
+echo "Test 3: SUCCESS"
 
 exit $?
