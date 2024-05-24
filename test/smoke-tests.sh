d3aa3baf3b59a2afc0b40413c26b8ebd5761c1b3
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2024-05-24 11:23:22 -0700

    Add a test case for this issue

--- test/smoke-tests.sh
+++ test/smoke-tests.sh
@@ -21,7 +21,7 @@ echo "Test 1: ldd-template"
 git clone https://github.com/pds-data-dictionaries/ldd-template.git ldd-repo/
 
 # Run lddtool
-lddtool-pkg/lddtool*/bin/lddtool -lpJ -V 1L00 ldd-repo/src/*IngestLDD.xml
+lddtool-pkg/lddtool*/bin/lddtool -lpJ ldd-repo/src/*IngestLDD.xml
 
 if [ $? -ne 0 ]; then
     echo "Test 1: FAILED"
@@ -36,7 +36,7 @@ git clone https://github.com/pds-data-dictionaries/ldd-cart.git ldd-repo/
 cd ldd-repo/; git submodule update --init --force --remote; cd ..
 
 # Run lddtool
-lddtool-pkg/lddtool*/bin/lddtool -lpJ -V 1L00 ldd-repo/src/dependencies/ldd-*/src/*IngestLDD.xml ldd-repo/src/*IngestLDD.xml
+lddtool-pkg/lddtool*/bin/lddtool -lpJ ldd-repo/src/dependencies/ldd-*/src/*IngestLDD.xml ldd-repo/src/*IngestLDD.xml
 
 if [ $? -ne 0 ]; then
     echo "Test 2: FAILED"
@@ -44,13 +44,26 @@ if [ $? -ne 0 ]; then
 fi
 echo "Test 2: SUCCESS"
 
+echo "Test 3: ldd-nucspec (#771)"
+# Run lddtool with downloaded data
+lddtool-pkg/lddtool*/bin/lddtool -lpJ test/data/771/PDS4_NUCSPEC_IngestLDD.xml
+diff PDS4_NUCSPEC_*_1100.sch test/data/771/expected/PDS4_NUCSPEC_1M00_1100.sch | egrep -v "PDS4 Schematron for Name Space Id:nucspec" | egrep -v "Generated from the PDS4 Information Model" > tmp.txt
+lines=$(wc -l tmp.txt | awk '{print $1}')
+
+if [ $lines -ne 2 ]; then
+    echo "Test 3: FAILED"
+    echo $lines
+    exit $?
+fi
+echo "Test 3: SUCCESS"
+
 # File count check
-echo "Test 3: Check file counts from files generated"
+echo "Test 4: Check file counts from files generated"
 count=$(ls -al PDS* | wc -l)
-if [ $count -ne 12 ]; then
+if [ $count -ne 18 ]; then
     echo "Test 3: FAILED"
     exit 1
 fi
-echo "Test 3: SUCCESS"
+echo "Test 4: SUCCESS"
 
 exit $?
