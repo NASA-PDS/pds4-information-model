8d0998b93b3f200f8ecd08356892ed495cdcfd7e
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2025-07-03 12:28:09 -0700

    Update secrets baseline

--- bin/generate-detect-secrets.sh
+++ bin/generate-detect-secrets.sh
@@ -8,6 +8,7 @@ detect-secrets scan . \
                --exclude-files '\.pre-commit-config\.yaml' \
                --exclude-files 'target' \
                --exclude-files 'docs/release/.*' \
-               --exclude-files 'model-ontology/src/ontology/Data/.*' \
+               --exclude-files 'model-ontology/src/ontology/.*' \
                --exclude-files 'model-dmdocument/src/main/java/gov/nasa/pds/model/plugin/.*' \
+               --exclude-files 'model-lddtool/src/main/resources/.*' \
                --exclude-files 'docs/namespace-registry/.*' > .secrets.baseline
